1ce5aeb60e5e950dfb22f40a1c2f3bfbde7891f2
Anton <altay77@inbox.ru>
2022-12-27 20:59:30 +0700

    solution 1_2

--- program.java
+++ program.java
@@ -1,3 +1,5 @@
+// Задача 1
+
 package java_projects;
 
 public class program {
@@ -18,5 +20,24 @@ public class program {
     if (a>0) merged = merged + word1.substring(i);
     if (b>0) merged = merged + word2.substring(i);
     System.out.println ( merged );
+    }
+}
+// Задача 2
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int copiInd = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val){
+                nums[copiInd] = nums[i];
+                copiInd += 1;
+            }
+        }
+        int k = copiInd;
+        while (copiInd < nums.length) {
+            nums[copiInd] = '_';
+            copiInd += 1;
+        }
+    return k;
     }
 }
